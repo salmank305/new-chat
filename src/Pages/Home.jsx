b336440af8dada4203e51333308274233344d791
salman305 <salmankhatik300591@gmail.com>
2022-12-12 13:11:55 +0530

    fanally done

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebar from '../Components/SideBar'
+import Chat from '../Components/Chat'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className="container">
+        <Sidebar/>
+        <Chat/>
+     
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBo8r9J9WgkZYW4tVcxk7Wbpc6qKww06fo",
+  authDomain: "new-chat-18e9f.firebaseapp.com",
+  projectId: "new-chat-18e9f",
+  storageBucket: "new-chat-18e9f.appspot.com",
+  messagingSenderId: "514476784462",
+  appId: "1:514476784462:web:d7418c8da55de9256afc1f"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth =getAuth()
+ export const storage = getStorage();
+ export const db =getFirestore();
